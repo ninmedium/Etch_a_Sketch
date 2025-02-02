504fbd4f510ec3f5530585f73f3b324ee483b8cc
Nigel <ninmedium@gmail.com>
2025-02-03 00:17:24 +0800

    Small grid boxes make a perfect square

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createGrid(gridNum) {
         gridHold = document.createElement("div");
         gridHold.style.aspectRatio = "1 / 1";
         gridHold.style.width = `${660 / gridNum}px`;
+        gridHold.style.boxSizing = "border-box";
         gridHold.style.border = "1px solid black"
         container.appendChild(gridHold);
     }
