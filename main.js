38739ddc8de5c6159c2b3f493cf332312e9a4974
Nigel <ninmedium@gmail.com>
2025-02-03 00:39:31 +0800

    Add class for gridbox styles
    - makes the JS file neater

--- main.js
+++ main.js
@@ -5,10 +5,8 @@ let container = document.querySelector("#container");
 function createGrid(gridNum) {
     for (let i=0; i < (gridNum * gridNum); i++) {
         gridHold = document.createElement("div");
-        gridHold.style.aspectRatio = "1 / 1";
+        gridHold.className = "gridbox";
         gridHold.style.width = `${660 / gridNum}px`;
-        gridHold.style.boxSizing = "border-box";
-        gridHold.style.border = "1px solid black"
         container.appendChild(gridHold);
     }
 }
